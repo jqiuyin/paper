58e22959a1aaf97fb6f4ce4535cfa1546d3ad9ba
qiuyin <943571574@qq.com>
2018-12-31 17:09:40 +0800

    add draw funcation

--- main/connectToAccess.py
+++ main/connectToAccess.py
@@ -2,7 +2,7 @@ import pypyodbc
 import pandas
 
 def getData():
-    str='Driver={Microsoft Access Driver (*.mdb,*.accdb)};DBQ=C:\\Users\\jiqy1\\Desktop\\CBDB_aw_20180831\\CBDB_aw_20180831\\CBDB_aw_20180831\\CBDB_20180831_DATA1.mdb'
+    str='Driver={Microsoft Access Driver (*.mdb,*.accdb)};DBQ=C:\\Users\\94357\\eclipse-workspace\\paper\\CBDB_ax_20180831\\CBDB_20180831_DATA1.mdb'
     db=pypyodbc.win_connect_mdb(str)
     curser=db.cursor()
     curser.execute("select t1.c_personid , t1.c_name_chn , t1.c_index_year , c_assoc_desc_chn , t2.c_name_chn  from BIOG_MAIN as t1 , BIOG_MAIN as t2 , ASSOC_DATA , ASSOC_CODES where (t2.c_personid = ASSOC_DATA.c_assoc_id) and (ASSOC_DATA.c_assoc_code = ASSOC_CODES.c_assoc_code) and (t1.c_personid = ASSOC_DATA.c_personid) and (t1.c_index_year>=960 and t1.c_index_year<=1279)")
--- main/drawGraph.py
+++ main/drawGraph.py
@@ -6,12 +6,27 @@ Created on 2018年12月24日
 import networkx as nx
 import matplotlib.pyplot as plt
 import main.cleanData as data
+
 data=data.CleanData()
 
 result=data.get_table("bad_one")
-print(result)
-G=nx.Graph()
-G.add_edge("a", "b")
-plt.figure()
-nx.draw(G,  node_color='y' , with_labels=True,node_size=800)
+#print(result[[1,4]])
+G=nx.MultiGraph()
+for row in result[[1,4]].iterrows():
+    #print(row[1][4])
+    G.add_edge(str(row[1][1]), str(row[1][4]))
+
+#print(nx.degree(G))
+#print([i[1]*30 for i in nx.degree(G)])
+# G.add_edge("a", "b")
+#print(nx.kamada_kawai_layout(G))
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+plt.figure()   
+locations=nx.kamada_kawai_layout(G)
+for people in nx.degree(G):
+    location=locations.get(people[0])
+    #print(location)
+    nx.draw_networkx_labels(G, pos={people[0]:location},labels={people[0]:people[0]}, font_size=people[1]**0.8)
+nx.draw_networkx(G,pos=nx.kamada_kawai_layout(G),with_labels=False,node_size=[i[1]**1.8 for i in nx.degree(G)])
 plt.show()
